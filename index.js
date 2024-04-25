d2c1f2302da562b2c1b124476dd4d275d47c31b1
mabalal <95362819+mabalal@users.noreply.github.com>
2024-04-25 20:57:38 +0100

    lab solved

--- index.js
+++ index.js
@@ -1,17 +1,62 @@
 class SortedList {
-  constructor() {}
+  constructor(items =[]) {
+    this.items = items;
+    this.length = items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    let left = 0;
+    let right = this.items.length;
 
-  get(pos) {}
+    // Binary search to find the correct position to insert the new item
+    while (left < right) {
+      const mid = Math.floor((left + right) / 2);
+      if (this.items[mid] < item) {
+        left = mid + 1;
+      } else {
+        right = mid;
+      }
+    }
+    this.items.splice(left, 0, item);
 
-  max() {}
+    // Update the length property
+    this.length++;
+  
+  }
 
-  min() {}
+  get(pos) {
+    if(pos < 0 || pos >= this.length) {
+      throw new Error("OutOfBounds");
+    }
+    return this.items[pos];
+  }
 
-  sum() {}
+  max() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return this.items[this.length-1];
+    }
+  }
 
-  avg() {}
+  min() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList")
+    } else {
+      return this.items[0];
+    }
+  }
+
+  sum() {
+    return this.items.reduce((acc, curr) => acc +curr, 0);
+  }
+
+  avg() {
+    if(this.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+    return this.sum() / this.items.length;
+  }
 }
-
+}
 module.exports = SortedList;
